1f14466c89cc76989fcc4e209529b1147d2edab1
Merouane Mohamed Elamine <59193074+merouane2000@users.noreply.github.com>
2022-12-06 00:19:48 +0100

    pics

--- src/components/Members.js
+++ src/components/Members.js
@@ -6,45 +6,43 @@ import CardMedia from "@mui/material/CardMedia";
 import MilitaryTechIcon from "@mui/icons-material/MilitaryTech";
 import CardMembershipIcon from "@mui/icons-material/CardMembership";
 import GradeIcon from "@mui/icons-material/Grade";
-import Avatar from "@mui/material/Avatar";
-import CardActions from "@mui/material/CardActions";
-import { Link } from "react-router-dom";
-import Button from "@mui/material/Button";
+
+
 
 const MembersSquad = [
   {
     familyName: "Meroaune",
     name: "Mohamed Elamine",
     role: "Leader",
-    img: "merouane.jpg",
+    img: "https://img.freepik.com/free-photo/3d-rendering-zoom-call-avatar_23-2149556778.jpg?w=740&t=st=1670282065~exp=1670282665~hmac=a7673cf0015b00e27883b62a23201bf93a8287f3909617d1e3d833f3a0c7c639",
     icon: <GradeIcon />,
   },
   {
     familyName: "Bellemdjahed ",
     name: "Abdelsamed",
     role: "co-Leader",
-    img: "https://img.freepik.com/free-photo/3d-rendering-zoom-call-avatar_23-2149556785.jpg",
+    img: "https://img.freepik.com/free-photo/3d-rendering-zoom-call-avatar_23-2149556785.jpg?w=740&t=st=1670280777~exp=1670281377~hmac=ac03b57d5316bfd5a122ba4ca537f428e08082cc17302e96e4c645870ba9e8f1",
     icon: <MilitaryTechIcon />,
   },
   {
     familyName: "Cherifi",
     name: "Miloud",
     role: "co-Leader",
-    img: "miloud.jpg",
+    img: "https://img.freepik.com/free-photo/3d-rendering-zoom-call-avatar_23-2149556782.jpg?w=740&t=st=1670281730~exp=1670282330~hmac=3827d29bb56dbc9b7dd56e70318bcffaac6bdf691ecd97acc8d70d7d4d8bbc29",
     icon: <MilitaryTechIcon />,
   },
   {
     familyName: "Fettah",
     name: "Lilia",
     role: "member",
-    img: "fettah.jpg",
+    img: "https://img.freepik.com/free-photo/3d-rendering-zoom-call-avatar_23-2149556786.jpg?t=st=1670282065~exp=1670282665~hmac=30f8749cd179fe106ada52f67b574324c1eaba9e1351f573b1015daea6053361",
     icon: <CardMembershipIcon />,
   },
   {
     familyName: "Mehloul",
     name: "Hind",
     role: "member",
-    img: "hind.jpg",
+    img: "https://img.freepik.com/free-photo/3d-rendering-zoom-call-avatar_23-2149556784.jpg",
     icon: <CardMembershipIcon />,
   },
 ];
